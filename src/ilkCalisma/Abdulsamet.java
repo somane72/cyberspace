5660ae4a6dc83c8ccb7ab365225e54566af47197
Abdulsamet <abdulsametozden0212@gmail.com>
2020-11-26 17:48:10 +0100

    mesaj

--- src/ilkCalisma/Abdulsamet.java
+++ src/ilkCalisma/Abdulsamet.java
@@ -1,7 +1,12 @@
 package ilkCalisma;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Abdulsamet {
     public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver","C:\\Users\\hp\\Documents\\selenium dependencies\\drivers\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
 
         System.out.println("Abdulsamet ilk commit");
     }
